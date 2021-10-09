41f5a87ab956bb6e0b0322e12048029c1a53b2c7
Etay Cohen <etaychn@gmail.com>
2021-10-09 11:24:48 +0300

    Basic usage of pcap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use pcap::{Capture, Device};
+
 fn main() {
-    println!("Hello, world!");
+    let mut cap = pcap::Capture::from_device("en1")
+        .unwrap()
+        .promisc(true)
+        .immediate_mode(true)
+        .rfmon(true)
+        .open()
+        .unwrap();
+
+    while let Ok(packet) = cap.next() {
+        println!("received packet! {:?}", packet);
+    }
 }
